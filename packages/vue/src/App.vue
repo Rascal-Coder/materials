9a275a8a8e0e691bea0485fc48a870d7245f10c5
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2023-10-24 23:22:51 +0800

    init(projects): init

---
+++ packages/vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AdminLayoutDemo  />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AdminLayoutDemo from './demos/admin-layout.vue';
+defineOptions({
+  name: 'App'
+});
+</script>
+
+<style scoped></style>
